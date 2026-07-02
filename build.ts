4457b3b264619cd2be43f63c706bff298f278650
philomagi <tooppoo56@gmail.com>
2026-07-02 07:12:02 +0000

    copy licences on build

--- build.ts
+++ build.ts
@@ -1,11 +1,19 @@
 import tailwind from "bun-plugin-tailwind";
+import { copyFile } from "node:fs/promises";
 import { rm } from "node:fs/promises";
 import path from "node:path";
 
+const root = process.cwd();
 const outdir = path.join(process.cwd(), "dist");
 await rm(outdir, { recursive: true, force: true });
 
 const entrypoints = [...new Bun.Glob("src/**/*.html").scanSync()];
+const staticFiles = [
+  {
+    source: "THIRD_PARTY_LICENSES.txt",
+    destination: "licenses.txt",
+  },
+];
 
 const result = await Bun.build({
   entrypoints,
@@ -18,6 +26,15 @@ const result = await Bun.build({
     "process.env.NODE_ENV": JSON.stringify("production"),
   },
 });
+
+for (const file of staticFiles) {
+  await copyFile(
+    path.join(root, file.source),
+    path.join(outdir, file.destination),
+  );
+
+  console.log(` dist/${file.destination}`);
+}
 
 for (const output of result.outputs) {
   console.log(
